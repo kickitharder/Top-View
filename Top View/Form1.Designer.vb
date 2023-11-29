e4714a601c59aa5d03fb4feff82ae2e2de089865
kickitharder <70810495+kickitharder@users.noreply.github.com>
2023-11-29 09:00:44 +0000

    Version 231129

---
+++ Top View/Form1.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.PbxView = New System.Windows.Forms.PictureBox()
+        CType(Me.PbxView, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PbxView
+        '
+        Me.PbxView.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PbxView.Location = New System.Drawing.Point(0, 0)
+        Me.PbxView.Name = "PbxView"
+        Me.PbxView.Size = New System.Drawing.Size(256, 145)
+        Me.PbxView.TabIndex = 1
+        Me.PbxView.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowText
+        Me.ClientSize = New System.Drawing.Size(256, 145)
+        Me.Controls.Add(Me.PbxView)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Top View"
+        Me.TopMost = True
+        CType(Me.PbxView, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PbxView As PictureBox
+End Class
---
+++ Top View/Form1.vb
@@ -0,0 +1,81 @@
+﻿Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+
+Public Class Form1
+    Dim FormName = "Top View (231129)"
+    Dim Camera As VideoCaptureDevice
+    Dim CamNo As Integer = -1
+    Dim FrameRate As Integer = 1
+    'Dim VideoCaptureDevice As VideoCaptureDevice
+    Dim Bmp As Bitmap
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Startup()
+    End Sub
+
+    Private Sub Startup()
+        Dim cameras As New VideoCaptureDeviceForm
+        Dim videoDevices = New FilterInfoCollection(FilterCategory.VideoInputDevice)
+        Me.Text = FormName
+        CamNo = -1
+
+        For n = 0 To videoDevices.Count - 1
+            If videoDevices(n).MonikerString.Contains("global") Then CamNo = n
+        Next
+
+        If CamNo >= 0 Then
+            Try
+                Camera = New VideoCaptureDevice(videoDevices(CamNo).MonikerString)
+                AddHandler Camera.NewFrame, New NewFrameEventHandler(AddressOf Captured)
+                Camera.Start()
+                FrameRate = 1
+            Catch ex As Exception
+                MsgBox("Unable to activate camera:" + vbCr + vbCr + videoDevices(CamNo).MonikerString)
+            End Try
+        Else
+            MsgBox("Camera not available.")
+        End If
+    End Sub
+
+    Private Sub Captured(ByVal sender As Object, ByVal eventArgs As NewFrameEventArgs)
+        If Not Me.Visible Then Exit Sub
+        FrameRate -= 1
+        If FrameRate Then Exit Sub
+        Me.TopMost = (Me.Height = 184)
+        PbxView.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+        PbxView.SizeMode = PictureBoxSizeMode.StretchImage
+        FrameRate = 1
+    End Sub
+
+    'eventArgs.Frame.Dispose()
+    'Bmp = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+    'PictureBox1.Image = DirectCast(eventArgs.Frame.Clone(), Bitmap)
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        StopCamera()
+    End Sub
+
+    Private Sub StopCamera()
+        If CamNo >= 0 Then
+            If Camera.IsRunning Then
+                Camera.SignalToStop()
+                Camera.WaitForStop()
+                Camera.Stop()
+            End If
+        End If
+    End Sub
+
+    Private Sub PbxView_DoubleClick(sender As Object, e As EventArgs) Handles PbxView.DoubleClick
+        Dim cameras As VideoCaptureDeviceForm = New VideoCaptureDeviceForm
+        StopCamera()
+        Me.Visible = False
+        If cameras.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Camera = cameras.VideoDevice
+            AddHandler Camera.NewFrame, New NewFrameEventHandler(AddressOf Captured)
+            Camera.Start()
+            CamNo = 1
+        End If
+        Me.Visible = True
+    End Sub
+End Class
